202ba76c27478497b1ae4443af3df99f48c4cd06
jpattanashetti <jpattanashetti9@gmail.com>
2025-04-15 09:49:00 +0530

    saved

---
+++ SimpleInterest.java
@@ -0,0 +1,10 @@
+public class SimpleInterest {
+    public static void main(String[] args) {
+        double p=20.0;
+        double t=10.0;
+        double r=5.0;
+        double SI=(p*t*r)/100;
+        System.out.println("Simple interest is: " + SI);
+
+    }
+}
